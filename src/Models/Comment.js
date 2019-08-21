227a93301db888f19a47a7ab74154f5847266af2
BeanFork <beanforkaccess@gmail.com>
2019-08-21 14:00:35 +0530

    Added the files

---
+++ src/Models/Comment.js
@@ -0,0 +1,18 @@
+createComment = function(postid,email,comment,username){
+  // create the object with email,comment,username
+
+  //update the db with the object as fields
+
+}
+
+editComment = function(postid,commentid,newcomment){
+  //check for postid and commentid
+
+  //update the comment field with the new comment
+}
+
+deleteComment = function(postid,commentid){
+    //check for postid and commentid 
+
+    //delete the comment and then commentid
+  }
