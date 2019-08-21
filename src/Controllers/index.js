0f52802b9bb39a0530189f5547a6e2df0621e663
aarthi <aarthisubha98@gmail.com>
2019-08-21 17:26:29 +0530

    Added index file in Controller

--- src/Controllers/index.js
+++ src/Controllers/index.js
@@ -1,4 +1,7 @@
 /**
- * get the Register.html from Views
- * In signUp case if register clicked then call registerUser function in 
+ * get the register.html from Views
+ * In signUp case if register clicked then call registerUser and userProfile function in user.js/Models
+ * In Login case if login button entered then call loginUser function in user.js/Models. Then direct to home.html
+ * In  case of Forgot password,direct to forgotPassword.html then call validateEmail,generateCode and validateCode function in user.js/Models
+ * 
  *  */ 
